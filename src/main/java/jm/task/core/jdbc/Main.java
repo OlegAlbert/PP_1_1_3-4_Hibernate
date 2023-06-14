f0b161e7dd5838e1c4d0f7d686a451a21cd3de13
Альберт Олег Витальевич <oleg.albert.99@mail.ru>
2023-06-14 15:54:32 +0300

    Правки

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -4,8 +4,6 @@ import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
 import jm.task.core.jdbc.util.Util;
 
-import java.sql.SQLException;
-
 public class Main {
     public static void main(String[] args) {
         Util.getConnection();
